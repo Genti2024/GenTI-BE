008c8d5b83956827cfe27beb8e3b4cb42d7f48ca
BYEONGRYEOL <lyulbyung@gmail.com>
2024-04-07 15:39:27 +0900

    Create: 누락된 PostResponseDto 추가

---
+++ src/main/java/com/gt/genti/dto/PostResponseDto.java
@@ -0,0 +1,31 @@
+package com.gt.genti.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostResponseDto {
+	Long postId;
+	Long userId;
+	String profileImageUrl;
+	List<String> postImageUrl;
+	String content;
+	Integer likes;
+	LocalDateTime createdAt;
+
+	public PostResponseDto(Long postId, Long userId, String profileImageUrl, List<String> postImageUrl, String content,
+		Integer likes, LocalDateTime createdAt) {
+		this.postId = postId;
+		this.userId = userId;
+		this.profileImageUrl = profileImageUrl;
+		this.postImageUrl = postImageUrl;
+		this.content = content;
+		this.likes = likes;
+		this.createdAt = createdAt;
+	}
+}

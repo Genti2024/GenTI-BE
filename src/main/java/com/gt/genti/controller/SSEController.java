6aa23a9c046bd0737c22729352f2b63c45bbe393
BYEONGRYEOL <lyulbyung@gmail.com>
2024-04-12 21:07:01 +0900

    Create: SSE 테스트 로직 구현

---
+++ src/main/java/com/gt/genti/controller/SSEController.java
@@ -0,0 +1,43 @@
+package com.gt.genti.controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.gt.genti.domain.User;
+import com.gt.genti.service.SSEService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/sse")
+public class SSEController {
+
+	private final SSEService sseService;
+
+	@GetMapping("")
+	public SseEmitter subscribe(@AuthenticationPrincipal User user) {
+		return sseService.subscribe(user.getId());
+	}
+
+	@GetMapping("/{id}")
+	public SseEmitter testSubscribe(@PathVariable Long id) {
+		return sseService.subscribe(id);
+	}
+
+	@PostMapping("/test/{id}")
+	public ResponseEntity<Void> test(@PathVariable Long id) {
+		sseService.notify(id, Map.of("테스트키1", "테스트밸류1", "테스트키2", "테스트밸류2"));
+		return ResponseEntity.ok().build();
+	}
+}

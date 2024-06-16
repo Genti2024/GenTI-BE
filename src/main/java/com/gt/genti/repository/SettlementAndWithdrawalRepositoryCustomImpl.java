31747f6680aeea3c64c507e0b38fc70d72617f01
BYEONGRYEOL <lyulbyung@gmail.com>
2024-06-16 18:58:13 +0900

    Fix: nativequery camelcase 제거

--- src/main/java/com/gt/genti/repository/SettlementAndWithdrawalRepositoryCustomImpl.java
+++ src/main/java/com/gt/genti/repository/SettlementAndWithdrawalRepositoryCustomImpl.java
@@ -26,7 +26,7 @@ public class SettlementAndWithdrawalRepositoryCustomImpl implements SettlementAn
 
 		String queryStr = "(SELECT s.id as id , true as isSettlement, s.reward as amount, s.created_at as createdAt, " +
 			"CASE WHEN wr.id IS NULL THEN 'AVAILABLE' ELSE wr.status END as status " +
-			"FROM Settlement s " +
+			"FROM settlement s " +
 			"LEFT OUTER JOIN withdraw_request wr ON wr.creator_id = :creatorId "
 			+ "LEFT JOIN picture_generate_response ON s.picture_generate_response_id = picture_generate_response.id " +
 			"UNION ALL " +

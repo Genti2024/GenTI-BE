9f0ec17b3fefe7eefb4f41534d1d4f63408205a6
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-03 08:49:15 +0900

    [Create] #20 sub-module root 추가 명시적 package scan에 활용

--- genti-api/src/main/java/com/gt/genti/GentiApplication.java
+++ genti-api/src/main/java/com/gt/genti/GentiApplication.java
@@ -4,12 +4,12 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import jakarta.annotation.PostConstruct;
 
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackageClasses = {CommonRoot.class, AuthRoot.class, DomainRoot.class, ExternalRoot.class}
+)
 public class GentiApplication {
 
 	public static void main(String[] args) {

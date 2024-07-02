e5d29da3ae21f9e9cc0a9abaa5c34734d9ef1591
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-03 08:54:47 +0900

    [Create] #20 Event pub-sub 구조 사용

---
+++ genti-external/src/main/java/com/gt/genti/discord/SignUpEventListener.java
@@ -0,0 +1,27 @@
+package com.gt.genti.discord;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@Profile("deploy")
+@RequiredArgsConstructor
+public class SignUpEventListener {
+
+    private final DiscordAppender discordAppender;
+
+    @EventListener
+    public void handleSignUpEvent(SignUpEvent event) {
+        discordAppender.signInAppend(
+                event.totalUserCount(),
+                event.name(),
+                event.email(),
+                event.socialPlatform(),
+                event.createdAt(),
+                event.imageUrl());
+    }
+
+}

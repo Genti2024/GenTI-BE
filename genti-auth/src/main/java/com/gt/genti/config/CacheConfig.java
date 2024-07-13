3e441975cacf944b2cf7388477365296d0fdfa5a
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-13 14:33:10 +0900

    Feat: apple public key 사용시 캐싱 필요, public key 미사용시 삭제요망

--- genti-auth/src/main/java/com/gt/genti/config/CacheConfig.java
+++ genti-auth/src/main/java/com/gt/genti/config/CacheConfig.java
@@ -6,11 +6,12 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import lombok.RequiredArgsConstructor;
@@ -23,14 +24,33 @@ public class CacheConfig {
 	private final RedisConnectionFactory redisConnectionFactory;
 
 	@Bean
+	@Primary
 	public CacheManager redisCacheManager() {
-		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-			.serializeKeysWith(SerializationPair.fromSerializer(new StringRedisSerializer()))
-			.serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+		RedisCacheConfiguration redisCacheConfiguration = generateCacheConfiguration()
 			.entryTtl(Duration.ofSeconds(60 * 60));
 
 		return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
 			.cacheDefaults(redisCacheConfiguration)
 			.build();
+	}
+
+	@Bean
+	public CacheManager oauthPublicKeyCacheManager(RedisConnectionFactory redisConnectionFactory) {
+		RedisCacheConfiguration redisCacheConfiguration = generateCacheConfiguration()
+			.entryTtl(Duration.ofDays(3L));
+		return RedisCacheManager.RedisCacheManagerBuilder
+			.fromConnectionFactory(redisConnectionFactory)
+			.cacheDefaults(redisCacheConfiguration)
+			.build();
+	}
+
+	private RedisCacheConfiguration generateCacheConfiguration() {
+		return RedisCacheConfiguration.defaultCacheConfig()
+			.serializeKeysWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(
+					new StringRedisSerializer()))
+			.serializeValuesWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(
+					new GenericJackson2JsonRedisSerializer()));
 	}
 }

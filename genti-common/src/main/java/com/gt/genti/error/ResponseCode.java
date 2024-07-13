afc309e3b1c8861ad3d2a1fde6795282505a0f90
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-13 14:31:40 +0900

    Feat: #26 apple oauth 관련 예외 추가

--- genti-common/src/main/java/com/gt/genti/error/ResponseCode.java
+++ genti-common/src/main/java/com/gt/genti/error/ResponseCode.java
@@ -1,5 +1,7 @@
 package com.gt.genti.error;
 
+import static com.gt.genti.constants.ErrorConstants.*;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,164 +18,176 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ResponseCode {
-	OK(true, HttpStatus.OK, null, null),
+	OK(null, HttpStatus.OK, true, null),
 	/**
 	 * 인증/인가
 	 */
-	TOKEN_NOT_PROVIDED(false, HttpStatus.UNAUTHORIZED, ErrorConstants.TOKEN_NOT_PROVIDED, "토큰이 전달되지 않았습니다."),
-	TOKEN_EXPIRED(false, HttpStatus.UNAUTHORIZED, ErrorConstants.TOKEN_EXPIRED, "토큰이 만료되었습니다."),
-	INVALID_TOKEN(false, HttpStatus.UNAUTHORIZED, ErrorConstants.INVALID_TOKEN, "유효하지 않은 토큰입니다."),
-	INSUFFICIENT_PERMISSIONS(false, HttpStatus.FORBIDDEN, ErrorConstants.INSUFFICIENT_PERMISSIONS, "접근 권한이 부족합니다."),
-	REFRESH_TOKEN_EXPIRED(false, HttpStatus.UNAUTHORIZED, ErrorConstants.REFRESH_TOKEN_EXPIRED, "리프레시 토큰이 만료되었습니다."),
-	REFRESH_TOKEN_INVALID(false, HttpStatus.UNAUTHORIZED, ErrorConstants.REFRESH_TOKEN_INVALID, "유효하지 않은 리프레시 토큰입니다."),
-	TOKEN_REFRESH_FAILED(false, HttpStatus.UNAUTHORIZED, ErrorConstants.TOKEN_REFRESH_FAILED,
+	TOKEN_NOT_PROVIDED(ErrorConstants.TOKEN_NOT_PROVIDED, HttpStatus.UNAUTHORIZED, false, "토큰이 전달되지 않았습니다."),
+	TOKEN_EXPIRED(ErrorConstants.TOKEN_EXPIRED, HttpStatus.UNAUTHORIZED, false, "토큰이 만료되었습니다."),
+	INVALID_TOKEN(ErrorConstants.INVALID_TOKEN, HttpStatus.UNAUTHORIZED, false, "유효하지 않은 토큰입니다."),
+	INSUFFICIENT_PERMISSIONS(ErrorConstants.INSUFFICIENT_PERMISSIONS, HttpStatus.FORBIDDEN, false, "접근 권한이 부족합니다."),
+	REFRESH_TOKEN_EXPIRED(ErrorConstants.REFRESH_TOKEN_EXPIRED, HttpStatus.UNAUTHORIZED, false, "리프레시 토큰이 만료되었습니다."),
+	REFRESH_TOKEN_INVALID(ErrorConstants.REFRESH_TOKEN_INVALID, HttpStatus.UNAUTHORIZED, false, "유효하지 않은 리프레시 토큰입니다."),
+	TOKEN_REFRESH_FAILED(ErrorConstants.TOKEN_REFRESH_FAILED, HttpStatus.UNAUTHORIZED, false,
 		"토큰 갱신에 실패했습니다."),
-	INVALID_REFRESH_TOKEN(false, HttpStatus.UNAUTHORIZED, ErrorConstants.INVALID_REFRESH_TOKEN,
+	INVALID_REFRESH_TOKEN(ErrorConstants.INVALID_REFRESH_TOKEN, HttpStatus.UNAUTHORIZED, false,
 		"토큰 갱신에 실패했습니다."),
-	Forbidden(false, HttpStatus.FORBIDDEN, ErrorConstants.Forbidden,
+	Forbidden(ErrorConstants.Forbidden, HttpStatus.FORBIDDEN, false,
 		"토큰 갱신에 실패했습니다."),
-	UnAuthorized(false, HttpStatus.UNAUTHORIZED, ErrorConstants.UnAuthorized,
+	UnAuthorized(ErrorConstants.UnAuthorized, HttpStatus.UNAUTHORIZED, false,
 		"인증되지 않은 사용자."),
+	EncryptAlgorithmDeprecated(ErrorConstants.UnHandledException, HttpStatus.INTERNAL_SERVER_ERROR, false,
+		"%s 암호화 과정 중 문제가 발생했습니다."),
 
 	/**
 	 * Oauth
 	 */
-	OauthProviderNotAllowed(false, HttpStatus.NOT_ACCEPTABLE, ErrorConstants.NotAllowedOauthProvider,
+	OauthProviderNotAllowed(ErrorConstants.OauthProviderNotAllowed, HttpStatus.NOT_ACCEPTABLE, false,
 		"허가되지 않은 oauth type %s"),
+	AppleOauthIdTokenIncorrect(ErrorConstants.AppleOauthIdTokenIncorrect, HttpStatus.BAD_REQUEST, false,
+		"Apple OAuth Identity Token 형식이 올바르지 않습니다."),
+	AppleOauthIdTokenExpired(ErrorConstants.AppleOauthIdTokenExpired, HttpStatus.BAD_REQUEST, false,
+		"Apple OAuth 로그인 중 Identity Token 유효기간이 만료됐습니다."),
+	AppleOauthIdTokenInvalid(ErrorConstants.AppleOauthIdTokenInvalid, HttpStatus.BAD_REQUEST, false,
+		"Apple OAuth Identity Token 값이 올바르지 않습니다."),
+	AppleOauthClaimInvalid(ErrorConstants.AppleOauthClaimInvalid, HttpStatus.BAD_REQUEST, false,
+		"Apple OAuth Claims 값이 올바르지 않습니다."),
+	AppleOauthPublicKeyInvalid(ErrorConstants.AppleOauthPublicKeyInvalid, HttpStatus.BAD_REQUEST, false,
+		"Apple OAuth 로그인 중 public key 생성에 문제가 발생했습니다."),
+
 	/**
 	 * 서버 오류 및 올바르지 않은 요청
 	 */
-	NotNullableEnum(false, HttpStatus.BAD_REQUEST, ErrorConstants.NotNullableEnum, " [%s] 값은 null 값을 허용하지 않습니다."),
-	DBToEnumFailed(false, HttpStatus.INTERNAL_SERVER_ERROR, ErrorConstants.DBToEnumFailed,
+	NotNullableEnum(ErrorConstants.NotNullableEnum, HttpStatus.BAD_REQUEST, false, " [%s] 값은 null 값을 허용하지 않습니다."),
+	DBToEnumFailed(ErrorConstants.DBToEnumFailed, HttpStatus.INTERNAL_SERVER_ERROR, false,
 		"문자열 -> Enum 변환 실패 enum : %s 입력된 값 : %s"),
-	HandlerMethodValidationException(false, HttpStatus.BAD_REQUEST, ErrorConstants.ControllerValidationError, "%s"),
-	UnrecognizedPropertyException(false, HttpStatus.BAD_REQUEST, ErrorConstants.UnrecognizedPropertyException,
+	HandlerMethodValidationException(ControllerValidationError, HttpStatus.BAD_REQUEST, false, "%s"),
+	UnrecognizedPropertyException(ErrorConstants.UnrecognizedPropertyException, HttpStatus.BAD_REQUEST, false,
 		"json property parsing 중 오류 발생, %s"),
-	InvalidDataAccessApiUsage(false, HttpStatus.BAD_REQUEST, ErrorConstants.InvalidDataAccessApiUsageException,
+	InvalidDataAccessApiUsage(InvalidDataAccessApiUsageException, HttpStatus.BAD_REQUEST, false,
 		"%s"),
-	NotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.NoHandlerFoundException,
+	NotFound(NoHandlerFoundException, HttpStatus.NOT_FOUND, false,
 		"요청 uri에 대한 핸들러가 없습니다."),
-	HttpRequestMethodNotSupportedException(false, HttpStatus.METHOD_NOT_ALLOWED, ErrorConstants.MethodNotSupported,
+	HttpRequestMethodNotSupportedException(MethodNotSupported, HttpStatus.METHOD_NOT_ALLOWED, false,
 		"not allowed methods, allowed methods : %s"),
-	MethodArgumentTypeMismatchException(false, HttpStatus.BAD_REQUEST, ErrorConstants.MethodArgumentTypeMismatch, "%s"),
-	MissingPathVariableException(false, HttpStatus.BAD_REQUEST, ErrorConstants.MissingPathVariableException,
+	MethodArgumentTypeMismatchException(MethodArgumentTypeMismatch, HttpStatus.BAD_REQUEST, false, "%s"),
+	MissingPathVariableException(ErrorConstants.MissingPathVariableException, HttpStatus.BAD_REQUEST, false,
 		"query param 에러 %s "),
-	UnHandledException(false, HttpStatus.INTERNAL_SERVER_ERROR, ErrorConstants.UnHandledException,
+	UnHandledException(ErrorConstants.UnHandledException, HttpStatus.INTERNAL_SERVER_ERROR, false,
 		"예기치 못한 문제가 발생했습니다. 오류내용 : %s"),
-	DiscordException(false, HttpStatus.INTERNAL_SERVER_ERROR, ErrorConstants.UnHandledException,
+	DiscordException(ErrorConstants.UnHandledException, HttpStatus.INTERNAL_SERVER_ERROR, false,
 		"디스코드 오류 발생"),
-	DiscordContentException(false, HttpStatus.INTERNAL_SERVER_ERROR, ErrorConstants.UnHandledException,
+	DiscordContentException(ErrorConstants.UnHandledException, HttpStatus.INTERNAL_SERVER_ERROR, false,
 		"디스코드 오류 발생"),
-	DiscordIOException(false, HttpStatus.INTERNAL_SERVER_ERROR, ErrorConstants.UnHandledException,
+	DiscordIOException(ErrorConstants.UnHandledException, HttpStatus.INTERNAL_SERVER_ERROR, false,
 		"디스코드 IO 오류 발생"),
-	DiscordAppenderException(false, HttpStatus.INTERNAL_SERVER_ERROR, ErrorConstants.UnHandledException,
+	DiscordAppenderException(ErrorConstants.UnHandledException, HttpStatus.INTERNAL_SERVER_ERROR, false,
 		"디스코드 IO 오류 발생"),
-	TimeOut(false,HttpStatus.REQUEST_TIMEOUT ,ErrorConstants.UnHandledException , "요청 시간이 초과되었습니다."),
+	TimeOut(ErrorConstants.UnHandledException, HttpStatus.REQUEST_TIMEOUT, false, "요청 시간이 초과되었습니다."),
 
 	/**
 	 * PictureGenerateRequest
 	 */
-	ActivePictureGenerateRequestNotExists(false, HttpStatus.NOT_FOUND,
-		ErrorConstants.ActivePictureGenerateRequestNotExists,
+	ActivePictureGenerateRequestNotExists(ErrorConstants.ActivePictureGenerateRequestNotExists, HttpStatus.NOT_FOUND,
+		false,
 		"현재 진행중인 요청이 없습니다."),
-	PictureGenerateRequestNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.PictureGenerateRequestNotFound,
+	PictureGenerateRequestNotFound(ErrorConstants.PictureGenerateRequestNotFound, HttpStatus.NOT_FOUND, false,
 		"사진 생성 요청을 찾지 못했습니다. 조회 조건 : %s"),
-	UnexpectedPictureGenerateRequestStatus(false, HttpStatus.CONFLICT,
-		ErrorConstants.UnexpectedPictureGenerateRequestStatus, "현재 사진생성요청의 상태가 [%s]임, 요청을 수행할 수 없습니다."),
-	NoPictureGenerateRequest(false, HttpStatus.NO_CONTENT, ErrorConstants.NoPictureGenerateRequest,
+	UnexpectedPictureGenerateRequestStatus(ErrorConstants.UnexpectedPictureGenerateRequestStatus, HttpStatus.CONFLICT,
+		false, "현재 사진생성요청의 상태가 [%s]임, 요청을 수행할 수 없습니다."),
+	NoPictureGenerateRequest(ErrorConstants.NoPictureGenerateRequest, HttpStatus.NO_CONTENT, false,
 		"사진 생성요청이 1개도 없습니다."),
-	PictureGenerateRequestAlreadyInProgress(false, HttpStatus.BAD_REQUEST,
-		ErrorConstants.PictureGenerateRequestAlreadyInProgress,
-		"이미 작업이 진행중인 요청은 수정이 불가합니다."),
-	PictureGenerateRequestNotAssignedToCreator(false, HttpStatus.BAD_REQUEST,
-		ErrorConstants.PictureGenerateRequestNotAssignedToCreator,
+	PictureGenerateRequestAlreadyInProgress(ErrorConstants.PictureGenerateRequestAlreadyInProgress,
+		HttpStatus.BAD_REQUEST, false, "이미 작업이 진행중인 요청은 수정이 불가합니다."),
+	PictureGenerateRequestNotAssignedToCreator(ErrorConstants.PictureGenerateRequestNotAssignedToCreator,
+		HttpStatus.BAD_REQUEST,
+		false,
 		"현재 공급자에게 매칭된 요청이 아닙니다."),
-	PictureGenerateRequestNotAcceptableDueToExpired(false, HttpStatus.BAD_REQUEST,
-		ErrorConstants.PictureGenerateRequestNotAcceptableDueToExpired, "수락 마감 시간을 초과하였습니다."),
-	PictureGenerateRequestVisibilityRestrictedToRequester(false, HttpStatus.FORBIDDEN,
-		ErrorConstants.OnlyRequesterCanViewRequest,
+	PictureGenerateRequestNotAcceptableDueToExpired(ErrorConstants.PictureGenerateRequestNotAcceptableDueToExpired,
+		HttpStatus.BAD_REQUEST,
+		false, "수락 마감 시간을 초과하였습니다."),
+	PictureGenerateRequestVisibilityRestrictedToRequester(OnlyRequesterCanViewRequest, HttpStatus.FORBIDDEN,
+		false,
 		"사진생성요청을 요청한 유저만 볼 수 있습니다."),
 	/**
 	 * User
 	 */
-	AlreadyActivatedUser(false, HttpStatus.BAD_REQUEST, ErrorConstants.AlreadyActivatedUser, "비활성화 되지 않은 상태의 유저입니다."),
-	CannotRestoreUser(false, HttpStatus.BAD_REQUEST, ErrorConstants.CannotRestoreUser,
+	AlreadyActivatedUser(ErrorConstants.AlreadyActivatedUser, HttpStatus.BAD_REQUEST, false, "비활성화 되지 않은 상태의 유저입니다."),
+	CannotRestoreUser(ErrorConstants.CannotRestoreUser, HttpStatus.BAD_REQUEST, false,
 		"탈퇴 후 한달이 지난 경우 재가입해야합니다. 찾은 userId : [%s]"),
-	UserNotLoggedIn(false, HttpStatus.UNAUTHORIZED, ErrorConstants.UserNotLoggedIn, "로그아웃되었습니다. 다시 로그인해주세요"),
-	WithDrawnUser(false, HttpStatus.BAD_REQUEST, ErrorConstants.WithDrawnUser, "탈퇴한 사용자입니다."),
-	UserNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.UserNotFound, "존재하지 않는 사용자입니다. 찾은 userId: [%s]"),
-	UserDeactivated(false, HttpStatus.BAD_REQUEST, ErrorConstants.UserDeactivated, "비활성화된 계정입니다."),
+	UserNotLoggedIn(ErrorConstants.UserNotLoggedIn, HttpStatus.UNAUTHORIZED, false, "로그아웃되었습니다. 다시 로그인해주세요"),
+	WithDrawnUser(ErrorConstants.WithDrawnUser, HttpStatus.BAD_REQUEST, false, "탈퇴한 사용자입니다."),
+	UserNotFound(ErrorConstants.UserNotFound, HttpStatus.NOT_FOUND, false, "존재하지 않는 사용자입니다. 찾은 userId: [%s]"),
+	UserDeactivated(ErrorConstants.UserDeactivated, HttpStatus.BAD_REQUEST, false, "비활성화된 계정입니다."),
 
 	/**
 	 * Creator
 	 */
-	CreatorNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.CreatorNotFound, "userId [%d] 에 해당하는 공급자를 찾을 수 없습니다."),
+	CreatorNotFound(ErrorConstants.CreatorNotFound, HttpStatus.NOT_FOUND, false, "userId [%d] 에 해당하는 공급자를 찾을 수 없습니다."),
 
 	/**
 	 * Picture
 	 */
-	PictureNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.PictureNotFound, "사진을 찾을 수 없습니다."),
-	PictureUserFaceNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.PictureUserFaceNotFound,
+	PictureNotFound(ErrorConstants.PictureNotFound, HttpStatus.NOT_FOUND, false, "사진을 찾을 수 없습니다."),
+	PictureUserFaceNotFound(ErrorConstants.PictureUserFaceNotFound, HttpStatus.NOT_FOUND, false,
 		"유저의 얼굴 사진을 찾지 못했습니다."),
-	PicturePoseNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.PicturePoseNotFound, "포즈 참고 사진을 찾지 못했습니다."),
-	PictureCompletedNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.PictureCompletedNotFound,
+	PicturePoseNotFound(ErrorConstants.PicturePoseNotFound, HttpStatus.NOT_FOUND, false, "포즈 참고 사진을 찾지 못했습니다."),
+	PictureCompletedNotFound(ErrorConstants.PictureCompletedNotFound, HttpStatus.NOT_FOUND, false,
 		"최종 완성 사진을 찾지 못했습니다."),
-	PictureCreatedByCreatorNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.PictureCreatedByCreatorNotFound,
+	PictureCreatedByCreatorNotFound(ErrorConstants.PictureCreatedByCreatorNotFound, HttpStatus.NOT_FOUND, false,
 		"공급자가 제출한 1차 완성 사진을 찾지 못했습니다."),
-	PictureProfileNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.PictureProfileNotFound,
+	PictureProfileNotFound(ErrorConstants.PictureProfileNotFound, HttpStatus.NOT_FOUND, false,
 		"해당하는 유저 프로필 사진을 찾지 못했습니다."),
-	UploadFileTypeNotAvailable(false, HttpStatus.NOT_ACCEPTABLE, ErrorConstants.UploadFileTypeNotAvailable,
+	UploadFileTypeNotAvailable(ErrorConstants.UploadFileTypeNotAvailable, HttpStatus.NOT_ACCEPTABLE, false,
 		"업로드 가능한 파일이 아닙니다."),
 
 	/**
 	 * PictureGenerateResponse
 	 */
-	PictureGenerateResponseNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.PictureGenerateResponseNotFound,
+	PictureGenerateResponseNotFound(ErrorConstants.PictureGenerateResponseNotFound, HttpStatus.NOT_FOUND, false,
 		"해당하는 사진생성응답을 찾을 수 없습니다."),
-	FinalPictureNotUploadedYet(false, HttpStatus.BAD_REQUEST, ErrorConstants.FinalPictureNotUploadedYet,
+	FinalPictureNotUploadedYet(ErrorConstants.FinalPictureNotUploadedYet, HttpStatus.BAD_REQUEST, false,
 		"최종 작업 사진이 제출되지 않았습니다."),
-	CreatorsPictureNotUploadedYet(false, HttpStatus.BAD_REQUEST, ErrorConstants.CreatorsPictureNotUploadedYet,
+	CreatorsPictureNotUploadedYet(ErrorConstants.CreatorsPictureNotUploadedYet, HttpStatus.BAD_REQUEST, false,
 		"공급자 작업 사진이 제출되지 않았습니다."),
-	RequestBlockedDueToPictureGenerateResponseStatus(false, HttpStatus.NOT_ACCEPTABLE,
-		ErrorConstants.PGRESStateException,
+	RequestBlockedDueToPictureGenerateResponseStatus(PGRESStateException, HttpStatus.NOT_ACCEPTABLE,
+		false,
 		"현재 사진생성응답 상태가 [%s] 이므로 해당 요청을 수행할 수 없습니다."),
-	SubmitBlockedDueToPictureGenerateResponseIsExpired(false, HttpStatus.BAD_REQUEST,
-		ErrorConstants.SubmitBlockedDueToPictureGenerateResponseIsExpired,
+	SubmitBlockedDueToPictureGenerateResponseIsExpired(
+		ErrorConstants.SubmitBlockedDueToPictureGenerateResponseIsExpired, HttpStatus.BAD_REQUEST,
+		false,
 		"제출 마감 시간을 초과하였습니다."),
-	AlreadyCompletedPictureGenerateResponse(false, HttpStatus.BAD_REQUEST, ErrorConstants.AlreadyCompletedResponse,
+	AlreadyCompletedPictureGenerateResponse(AlreadyCompletedResponse, HttpStatus.BAD_REQUEST, false,
 		"이미 처리 완료된 응답입니다."),
 	/**
 	 * Report
 	 */
-	ReportNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.ReportNotFound, "해당 report 건을 찾지 못했습니다."),
-
+	ReportNotFound(ErrorConstants.ReportNotFound, HttpStatus.NOT_FOUND, false, "해당 report 건을 찾지 못했습니다."),
 	/**
 	 * Deposit
 	 */
-	DepositNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.DepositNotFound, "사용자의 포인트 정보를 불러올 수 없습니다."),
-	AddPointAmountCannotBeMinus(false, HttpStatus.BAD_REQUEST, ErrorConstants.AddPointAmountCannotBeMinus,
+	DepositNotFound(ErrorConstants.DepositNotFound, HttpStatus.NOT_FOUND, false, "사용자의 포인트 정보를 불러올 수 없습니다."),
+	AddPointAmountCannotBeMinus(ErrorConstants.AddPointAmountCannotBeMinus, HttpStatus.BAD_REQUEST, false,
 		"적립될 포인트는 음수일 수 없습니다."),
-	NotEnoughBalance(false, HttpStatus.NOT_ACCEPTABLE, ErrorConstants.NotEnoughBalance,
+	NotEnoughBalance(ErrorConstants.NotEnoughBalance, HttpStatus.NOT_ACCEPTABLE, false,
 		"해당 공급자의 출금가능 잔액이 부족하여 요청을 완료할 수 없습니다."),
-
 	/**
 	 * WithdrawRequest && Settlement
 	 */
-	NoSettlementForWithdrawalException(false, HttpStatus.BAD_REQUEST, ErrorConstants.NoSettlementForWithdrawalException,
+	NoSettlementForWithdrawalException(ErrorConstants.NoSettlementForWithdrawalException, HttpStatus.BAD_REQUEST, false,
 		"출금 가능한 정산 내역이 없습니다."),
-	WithdrawRequestNotFound(false, HttpStatus.NOT_FOUND, ErrorConstants.WithdrawRequestNotFound,
+	WithdrawRequestNotFound(ErrorConstants.WithdrawRequestNotFound, HttpStatus.NOT_FOUND, false,
 		"해당 출금 요청을 찾을 수 없습니다."),
+	HttpMessageNotReadableException(InValidFormat, HttpStatus.BAD_REQUEST, false, "잘못된 입력 : %s"),
+	FileTypeNotProvided(ErrorConstants.FileTypeNotProvided, HttpStatus.BAD_REQUEST, false, "파일 형식이 주어지지 않았습니다.");
 
-	HttpMessageNotReadableException(false, HttpStatus.BAD_REQUEST, ErrorConstants.InValidFormat, "잘못된 입력 : %s"),
-	FileTypeNotProvided(false, HttpStatus.BAD_REQUEST, ErrorConstants.FileTypeNotProvided, "파일 형식이 주어지지 않았습니다.");
-
-	private final boolean success;
-	private final HttpStatusCode httpStatusCode;
 	private final String errorCode;
+	private final HttpStatusCode httpStatusCode;
+	private final boolean success;
 	private final String errorMessage;
 
 	public String getErrorMessage(Object... args) {
-		if(this.success){
+		if (this.success) {
 			return null;
 		}
 		int placeholderCount = countPlaceholders(errorMessage);

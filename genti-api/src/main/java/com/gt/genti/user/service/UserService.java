41d65c617dc50f4ff56c0d59f07d48408ae2520e
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-13 14:32:36 +0900

    Del: 미사용,중복로직 함수 삭제 및 #26 로그인/로그아웃 기능 authservice로 분리

--- genti-api/src/main/java/com/gt/genti/user/service/UserService.java
+++ genti-api/src/main/java/com/gt/genti/user/service/UserService.java
@@ -1,15 +1,11 @@
 package com.gt.genti.user.service;
 
-import static com.gt.genti.user.service.validator.UserValidator.*;
-
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Function;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,24 +14,18 @@ import com.gt.genti.creator.repository.CreatorRepository;
 import com.gt.genti.deposit.service.DepositService;
 import com.gt.genti.error.ExpectedException;
 import com.gt.genti.error.ResponseCode;
-import com.gt.genti.jwt.JwtTokenProvider;
 import com.gt.genti.picture.completed.repository.PictureCompletedRepository;
 import com.gt.genti.picture.dto.response.CommonPictureResponseDto;
 import com.gt.genti.picture.profile.model.PictureProfile;
 import com.gt.genti.picture.service.PictureService;
-import com.gt.genti.user.dto.request.SocialLoginRequest;
 import com.gt.genti.user.dto.request.UserInfoUpdateRequestDto;
 import com.gt.genti.user.dto.request.UserRoleUpdateRequestDto;
 import com.gt.genti.user.dto.request.UserStatusUpdateRequestDto;
-import com.gt.genti.user.dto.response.SocialLoginResponse;
 import com.gt.genti.user.dto.response.UserFindByAdminResponseDto;
 import com.gt.genti.user.dto.response.UserFindResponseDto;
-import com.gt.genti.user.model.OauthPlatform;
 import com.gt.genti.user.model.User;
 import com.gt.genti.user.model.UserRole;
 import com.gt.genti.user.repository.UserRepository;
-import com.gt.genti.user.service.social.SocialLoginContext;
-import com.gt.genti.util.HttpRequestUtil;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
@@ -51,39 +41,12 @@ public class UserService {
 	private final DepositService depositService;
 	private final CreatorRepository creatorRepository;
 	private final PictureCompletedRepository pictureCompletedRepository;
-	private final SocialLoginContext socialLoginContext;
-	private final JwtTokenProvider jwtTokenProvider;
 
-	public SocialLoginResponse login(final SocialLoginRequest request) {
-		return socialLoginContext.doLogin(request);
-	}
-
-	public HttpHeaders getOauthRedirect(OauthPlatform oauthPlatform){
-		return HttpRequestUtil.createRedirectHttpHeader(socialLoginContext.getAuthUri(oauthPlatform));
-	}
-
-	public Boolean logout(final Long userId) {
-		User foundUser = getUserByUserId(userId);
-		validateUserAuthorization(foundUser.getId(), userId);
-		jwtTokenProvider.deleteRefreshToken(userId);
-		return true;
-	}
-
-	
 	public UserFindResponseDto getUserInfo(Long userId) {
 		User foundUser = getUserByUserId(userId);
-		List<CommonPictureResponseDto> profilePictureResponseList = null;
-		if (!foundUser.getPictureProfileList().isEmpty()) {
-			profilePictureResponseList = foundUser.getPictureProfileList()
-				.stream()
-				.map(CommonPictureResponseDto::of)
-				.toList();
-		}
-
 		return UserFindResponseDto.of(foundUser);
 	}
 
-	
 	public UserFindResponseDto updateUserInfo(Long userId, UserInfoUpdateRequestDto userInfoUpdateRequestDto) {
 		User foundUser = getUserByUserId(userId);
 		foundUser.updateName(userInfoUpdateRequestDto.getUserName());
@@ -108,7 +71,6 @@ public class UserService {
 			.build();
 	}
 
-	
 	public Boolean updateUserRole(Long userId, UserRoleUpdateRequestDto userRoleUpdateRequestDto) {
 		User foundUser = getUserByUserId(userId);
 		UserRole userRole = userRoleUpdateRequestDto.getUserRole();
@@ -124,21 +86,18 @@ public class UserService {
 		return true;
 	}
 
-	
 	public Boolean deleteUserSoft(Long userId) {
 		User foundUser = getUserByUserId(userId);
 		foundUser.softDelete();
 		return true;
 	}
 
-	
 	public Boolean restoreSoftDeletedUser(Long userId) {
 		User foundUser = getUserByUserId(userId);
 		foundUser.restore();
 		return true;
 	}
 
-	
 	public Boolean updateUserStatus(Long userId, UserStatusUpdateRequestDto userStatusUpdateRequestDto) {
 		User foundUser = getUserByUserId(userId);
 		foundUser.updateStatus(userStatusUpdateRequestDto.getUserStatus());
@@ -163,15 +122,6 @@ public class UserService {
 	private User getUserByUserId(Long userId) {
 		return userRepository.findById(userId)
 			.orElseThrow(() -> ExpectedException.withLogging(ResponseCode.UserNotFound, userId.toString()));
-	}
-
-	public Optional<User> findOptionalUser(String email) {
-		return userRepository.findByEmail(email);
-	}
-
-	public User createNewUser(User user) {
-		depositService.createDeposit(user);
-		return userRepository.save(user);
 	}
 
 	@NotNull

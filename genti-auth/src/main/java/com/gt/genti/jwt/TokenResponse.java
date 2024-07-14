0dd26273c75f3cecd7d19a5c2e7b03c72c9e9fb8
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-15 02:02:06 +0900

    Fix: #26 토큰 응답에 email 추가

--- genti-auth/src/main/java/com/gt/genti/jwt/TokenResponse.java
+++ genti-auth/src/main/java/com/gt/genti/jwt/TokenResponse.java
@@ -2,8 +2,13 @@ package com.gt.genti.jwt;
 
 import com.gt.genti.constants.JWTConstants;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "Genti 토큰 응답 Dto")
 public record TokenResponse(
+	@Schema(description ="액세스 토큰", example = "defaef.32faeE@.szfes!")
 	String accessToken,
+	@Schema(description ="리프레시 토큰", example = "defaef.32faeE@.szfes!")
 	String refreshToken
 ) {
 	public static TokenResponse of(String accessToken, String refreshToken) {

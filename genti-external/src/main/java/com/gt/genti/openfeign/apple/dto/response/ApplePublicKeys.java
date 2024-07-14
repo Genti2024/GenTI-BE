bac37aca654747e44ac5c80e74cbefb733ca02f2
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-14 23:12:51 +0900

    Fix: #26 예외시 ExpectedException 발생

--- genti-external/src/main/java/com/gt/genti/openfeign/apple/dto/response/ApplePublicKeys.java
+++ genti-external/src/main/java/com/gt/genti/openfeign/apple/dto/response/ApplePublicKeys.java
@@ -1,6 +1,10 @@
 package com.gt.genti.openfeign.apple.dto.response;
 
 import java.util.List;
+
+import com.gt.genti.error.ExpectedException;
+import com.gt.genti.error.ResponseCode;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,13 +15,13 @@ import lombok.NoArgsConstructor;
 @Getter
 public class ApplePublicKeys {
 
-    private List<ApplePublicKey> keys;
+	private List<ApplePublicKey> keys;
 
-    public ApplePublicKey getMatchesKey(String alg, String kid) {
-        return this.keys
-                .stream()
-                .filter(k -> k.getAlg().equals(alg) && k.getKid().equals(kid))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Apple JWT 값의 alg, kid 정보가 올바르지 않습니다."));
-    }
+	public ApplePublicKey getMatchesKey(String alg, String kid) {
+		return this.keys
+			.stream()
+			.filter(k -> k.getAlg().equals(alg) && k.getKid().equals(kid))
+			.findFirst()
+			.orElseThrow(() -> ExpectedException.withLogging(ResponseCode.AppleOauthJwtValueInvalid));
+	}
 }

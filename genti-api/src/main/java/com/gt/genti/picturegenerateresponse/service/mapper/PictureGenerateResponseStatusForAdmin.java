6d38953bfbc1fe20f447e8c9076deb7b31724f40
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-19 20:48:14 +0900

    Rename: #50

--- genti-api/src/main/java/com/gt/genti/picturegenerateresponse/service/mapper/PictureGenerateResponseStatusForAdmin.java
+++ genti-api/src/main/java/com/gt/genti/picturegenerateresponse/service/mapper/PictureGenerateResponseStatusForAdmin.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum PictureGenerateResponseStatusForAdminMatched implements ConvertableEnum {
+public enum PictureGenerateResponseStatusForAdmin implements ConvertableEnum {
 	BEFORE_WORK("BEFORE_WORK", "작업 대기"),
 	IN_PROGRESS("IN_PROGRESS", "작업 중"),
 	COMPLETED("COMPLETED", "작업 완료");

82d977847b0dcc0b363d2c7adbcab90f066e9c0c
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-19 20:49:00 +0900

    Rename: #50

--- genti-api/src/main/java/com/gt/genti/picturegenerateresponse/service/mapper/PictureGenerateResponseStatusMapper.java
+++ genti-api/src/main/java/com/gt/genti/picturegenerateresponse/service/mapper/PictureGenerateResponseStatusMapper.java
@@ -12,33 +12,33 @@ import com.gt.genti.picturegenerateresponse.model.PictureGenerateResponseStatus;
 
 public class PictureGenerateResponseStatusMapper {
 
-	private static final Map<PictureGenerateResponseStatus, PictureGenerateResponseStatusForAdminMatched> statusToAdminMatchedMapping = new EnumMap<>(
+	private static final Map<PictureGenerateResponseStatus, PictureGenerateResponseStatusForAdmin> statusToAdminMatchedMapping = new EnumMap<>(
 		PictureGenerateResponseStatus.class);
-	private static final Map<PictureGenerateResponseStatusForAdminMatched, PictureGenerateResponseStatus> AdminMatchedToStatusMapping = new EnumMap<>(
-		PictureGenerateResponseStatusForAdminMatched.class);
+	private static final Map<PictureGenerateResponseStatusForAdmin, PictureGenerateResponseStatus> AdminMatchedToStatusMapping = new EnumMap<>(
+		PictureGenerateResponseStatusForAdmin.class);
 
 	static {
-		List<SimpleEntry<PictureGenerateResponseStatus, PictureGenerateResponseStatusForAdminMatched>> mappings = Arrays.asList(
-			new SimpleEntry<>(BEFORE_WORK, PictureGenerateResponseStatusForAdminMatched.BEFORE_WORK),
-			new SimpleEntry<>(SUBMITTED_FIRST, PictureGenerateResponseStatusForAdminMatched.BEFORE_WORK),
-			new SimpleEntry<>(ADMIN_BEFORE_WORK, PictureGenerateResponseStatusForAdminMatched.BEFORE_WORK),
-			new SimpleEntry<>(ADMIN_IN_PROGRESS, PictureGenerateResponseStatusForAdminMatched.IN_PROGRESS),
-			new SimpleEntry<>(SUBMITTED_FINAL, PictureGenerateResponseStatusForAdminMatched.COMPLETED),
-			new SimpleEntry<>(REPORTED, PictureGenerateResponseStatusForAdminMatched.COMPLETED),
-			new SimpleEntry<>(COMPLETED, PictureGenerateResponseStatusForAdminMatched.COMPLETED)
+		List<SimpleEntry<PictureGenerateResponseStatus, PictureGenerateResponseStatusForAdmin>> mappings = Arrays.asList(
+			new SimpleEntry<>(BEFORE_WORK, PictureGenerateResponseStatusForAdmin.BEFORE_WORK),
+			new SimpleEntry<>(SUBMITTED_FIRST, PictureGenerateResponseStatusForAdmin.BEFORE_WORK),
+			new SimpleEntry<>(ADMIN_BEFORE_WORK, PictureGenerateResponseStatusForAdmin.BEFORE_WORK),
+			new SimpleEntry<>(ADMIN_IN_PROGRESS, PictureGenerateResponseStatusForAdmin.IN_PROGRESS),
+			new SimpleEntry<>(SUBMITTED_FINAL, PictureGenerateResponseStatusForAdmin.COMPLETED),
+			new SimpleEntry<>(REPORTED, PictureGenerateResponseStatusForAdmin.COMPLETED),
+			new SimpleEntry<>(COMPLETED, PictureGenerateResponseStatusForAdmin.COMPLETED)
 		);
 
-		for (SimpleEntry<PictureGenerateResponseStatus, PictureGenerateResponseStatusForAdminMatched> mapping : mappings) {
+		for (SimpleEntry<PictureGenerateResponseStatus, PictureGenerateResponseStatusForAdmin> mapping : mappings) {
 			statusToAdminMatchedMapping.put(mapping.getKey(), mapping.getValue());
 			AdminMatchedToStatusMapping.put(mapping.getValue(), mapping.getKey());
 		}
 	}
 
-	public static PictureGenerateResponseStatusForAdminMatched toForAdminMatched(PictureGenerateResponseStatus status) {
+	public static PictureGenerateResponseStatusForAdmin toForAdminMatched(PictureGenerateResponseStatus status) {
 		return statusToAdminMatchedMapping.get(status);
 	}
 
-	public static PictureGenerateResponseStatus toStatus(PictureGenerateResponseStatusForAdminMatched userStatus) {
+	public static PictureGenerateResponseStatus toStatus(PictureGenerateResponseStatusForAdmin userStatus) {
 		return AdminMatchedToStatusMapping.get(userStatus);
 	}
 }

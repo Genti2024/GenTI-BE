a50fc51f641b1b3637746093bb5e8beb5813c0b4
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-20 18:36:10 +0900

    Feat: #16 #50 담당자 변경 Api Dto 수정

--- genti-api/src/main/java/com/gt/genti/picturegenerateresponse/dto/request/PGRESUpdateAdminInChargeRequestDto.java
+++ genti-api/src/main/java/com/gt/genti/picturegenerateresponse/dto/request/PGRESUpdateAdminInChargeRequestDto.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Schema(name = "[PGRES][Admin] 사진생성응답 담당 어드민 변경 Dto")
 public class PGRESUpdateAdminInChargeRequestDto {
+	@Schema(description = "담당자를 설정하고자 하는 사진생성응답 DB ID", example = "1")
+	Long pictureGenerateResponseId;
 	@Schema(description = "담당자 이름", example = "안재욱")
 	String adminInCharge;
 }

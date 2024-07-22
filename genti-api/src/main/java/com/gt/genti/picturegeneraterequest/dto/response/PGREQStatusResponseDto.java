753bfc9dcf11ca0202d72175e1b040458a7c6010
BYEONGRYEOL <lyulbyung@gmail.com>
2024-07-22 22:01:30 +0900

    Fix: #16 #50 도메인 로직 변경으로 swagger 문서 수정

--- genti-api/src/main/java/com/gt/genti/picturegeneraterequest/dto/response/PGREQStatusResponseDto.java
+++ genti-api/src/main/java/com/gt/genti/picturegeneraterequest/dto/response/PGREQStatusResponseDto.java
@@ -20,7 +20,7 @@ public class PGREQStatusResponseDto {
 		"IN_PROGRESS : 작업이 진행 중" + "<br/>" +
 		"AWAIT_USER_VERIFICATION : 완료되었고, 사용자가 확인한 적이 없어 확인을 필요로 함" + "<br/>" +
 		"CANCELED : 1.공급자가 주문을 받아놓고 회원탈퇴한경우 2.12시간동안 주문을 아무도 받지 않을 경우, 3. 공급자가 주문을 받고 노쇼한 경우 취소되어 FE 분기처리" + "<br/>" +
-		"COMPLETED : 완료되었고, 사용자가 이미 확인함")
+		"NEW_REQUEST_AVAILABLE : 이전 주문이 완료되었거나 주문한적이 없는 등 새로운 요청 생성이 가능한 상태이다.")
 	PictureGenerateRequestStatusForUser status;
 
 	@Schema(description = "사진생성응답 객체, status 값이 AWAIT_USER_VERIFICATION 가 아니면 null", example = "1", nullable = true)

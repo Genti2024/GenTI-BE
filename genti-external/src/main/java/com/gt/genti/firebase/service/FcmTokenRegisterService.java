d5c1835b3448030e00bde5418cf2efc7a7f65006
BYEONGRYEOL <lyulbyung@gmail.com>
2024-08-04 17:51:11 +0900

    Feat: #72 유저 Oauth 로그인시 FCM 토큰 등록 로직

---
+++ genti-external/src/main/java/com/gt/genti/firebase/service/FcmTokenRegisterService.java
@@ -0,0 +1,40 @@
+package com.gt.genti.firebase.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gt.genti.error.ExpectedException;
+import com.gt.genti.error.ResponseCode;
+import com.gt.genti.fcm.model.FcmToken;
+import com.gt.genti.fcm.repository.FcmTokenRepository;
+import com.gt.genti.firebase.dto.request.FcmTokenSaveOrUpdateRequestDto;
+import com.gt.genti.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class FcmTokenRegisterService {
+
+	private final FcmTokenRepository fcmTokenRepository;
+	private final UserRepository userRepository;
+
+	public void registerFcmToken(final FcmTokenSaveOrUpdateRequestDto fcmTokenSaveOrUpdateRequestDto) {
+		final Long memberId = fcmTokenSaveOrUpdateRequestDto.getUserId();
+		final String token = fcmTokenSaveOrUpdateRequestDto.getToken();
+
+		checkUserExists(memberId);
+		fcmTokenRepository.findByUserId(memberId)
+			.ifPresentOrElse(
+				foundFcmToken -> foundFcmToken.update(token),
+				() -> fcmTokenRepository.save(new FcmToken(token, memberId))
+			);
+	}
+
+	private void checkUserExists(final Long userId) {
+		if (!userRepository.existsById(userId)) {
+			throw ExpectedException.withLogging(ResponseCode.UserNotFound, userId);
+		}
+	}
+}

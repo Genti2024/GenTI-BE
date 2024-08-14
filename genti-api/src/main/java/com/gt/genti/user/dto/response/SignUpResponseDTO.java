35c426f4bcc3cb486b483b90e195ffb9f24228c7
LeeJaehyung <540900@naver.com>
2024-08-15 04:35:03 +0900

    Fix: #118

---
+++ genti-api/src/main/java/com/gt/genti/user/dto/response/SignUpResponseDTO.java
@@ -0,0 +1,34 @@
+package com.gt.genti.user.dto.response;
+
+import com.gt.genti.user.model.OauthPlatform;
+import com.gt.genti.user.model.Sex;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Schema(name = "[User][User] 회원가입 응답 dto")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SignUpResponseDTO {
+    @Schema(description = "이메일", example = "woogie@google.com")
+    String email;
+    @Schema(description = "소셜 로그인 타입", example = "KAKAO")
+    OauthPlatform lastLoginOauthPlatform;
+    @Schema(description = "닉네임", example = "다정한 모래")
+    String nickname;
+    @Schema(description = "태어난 년도", example = "1999")
+    String birthDate;
+    @Schema(description = "성별", example = "남")
+    Sex sex;
+
+    @Builder
+    public SignUpResponseDTO(String email, OauthPlatform lastLoginOauthPlatform, String nickname, String birthDate, Sex sex) {
+        this.email = email;
+        this.lastLoginOauthPlatform = lastLoginOauthPlatform;
+        this.nickname = nickname;
+        this.birthDate = birthDate;
+        this.sex = sex;
+    }
+}

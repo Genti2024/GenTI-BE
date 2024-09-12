84a66e36dc97c574dd568d6f276198e6bbc10ef6
LeeJaehyung <540900@naver.com>
2024-09-12 17:07:55 +0900

    Fix: #156 생년 숫자 변환 로직 수정, SecurityConfig 수정

--- genti-api/src/main/java/com/gt/genti/openchat/service/OpenChatService.java
+++ genti-api/src/main/java/com/gt/genti/openchat/service/OpenChatService.java
@@ -9,6 +9,7 @@ import com.gt.genti.openchat.repository.OpenChatRepository;
 import com.gt.genti.user.model.User;
 import com.gt.genti.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,14 +18,14 @@ import static com.gt.genti.openchat.model.OpenChatType.*;
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class OpenChatService {
     private final OpenChatRepository openChatRepository;
     private final UserRepository userRepository;
 
     public OpenChatInfoResponseDto getOpenChatUrl(Long userId){
         User foundUser = getUserById(userId);
-        int userBirthYear = parseBirthYear(foundUser.getBirthYear());
-        OpenChatType openChatType = getOpenChatType(userBirthYear);
+        OpenChatType openChatType = getOpenChatType(foundUser);
 
         if(openChatType == YB || openChatType == OB) {
             OpenChat openChat = openChatRepository.findByType(openChatType);
@@ -61,20 +62,18 @@ public class OpenChatService {
                 .orElseThrow(() -> ExpectedException.withLogging(ResponseCode.UserNotFound, userId));
     }
 
-    private int parseBirthYear(String birthYear) {
-        try {
-            return Integer.parseInt(birthYear);
+    private OpenChatType getOpenChatType(User user) {
+        try{
+            int userBirthYear = Integer.parseInt(user.getBirthYear());
+            if (userBirthYear >= 2001) {
+                return YB;
+            } else if (userBirthYear <= 1974) {
+                return OB;
+            } else {
+                return NONE;
+            }
         } catch (NumberFormatException | NullPointerException e) {
-            return 2000;
-        }
-    }
-
-    private OpenChatType getOpenChatType(int userBirthYear) {
-        if (userBirthYear >= 2001) {
-            return YB;
-        } else if (userBirthYear <= 1974) {
-            return OB;
-        } else {
+            log.info("사용자 생년의 숫자 변환 시 문제가 발생했습니다.", e);
             return NONE;
         }
     }

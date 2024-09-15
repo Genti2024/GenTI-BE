15f43457873ca7426115853ccbebeca509600f29
BYEONGRYEOL <lyulbyung@gmail.com>
2024-09-15 14:33:36 +0900

    Fix: #163 swagger 문서화 추가

--- genti-api/src/main/java/com/gt/genti/admin/controller/PushNotificationController.java
+++ genti-api/src/main/java/com/gt/genti/admin/controller/PushNotificationController.java
@@ -1,4 +1,4 @@
-package com.gt.genti.admin;
+package com.gt.genti.admin.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.gt.genti.admin.api.PushNotificationApi;
 import com.gt.genti.admin.dto.request.SendPushRequestDto;
 import com.gt.genti.admin.service.PushNotificationService;
 import com.gt.genti.error.ExpectedException;
@@ -21,15 +23,16 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/push")
-public class PushNotificationController {
+public class PushNotificationController implements PushNotificationApi {
 
 	private final PushNotificationService pushNotificationService;
 
 	@Value("${openchat.admin-secret-key}")
 	private String ADMIN_SECRET_KEY;
 
+	@Override()
 	@PostMapping("/update")
-	public ResponseEntity<GentiResponse.ApiResult<Boolean>> modifyOpenChatInfo(
+	public ResponseEntity<GentiResponse.ApiResult<Boolean>> sendUpdatePush(
 		@RequestHeader(value = "Admin-Secret-Key") String adminSecretKey,
 		@RequestBody SendPushRequestDto sendPushRequestDto
 	) {
@@ -41,15 +44,17 @@ public class PushNotificationController {
 		}
 	}
 
+	@Override
 	@PostMapping("/test")
 	public ResponseEntity<GentiResponse.ApiResult<Boolean>> test(
 		@RequestHeader(value = "Admin-Secret-Key") String adminSecretKey,
-		@RequestBody SendPushRequestDto sendPushRequestDto
+		@RequestBody SendPushRequestDto sendPushRequestDto,
+		@RequestParam(name = "email") String email
 	) {
 		if (!ADMIN_SECRET_KEY.equals(adminSecretKey)) {
 			throw ExpectedException.withLogging(ResponseCode.InvalidOpenChatSecretKey);
 		} else {
-			pushNotificationService.test(sendPushRequestDto);
+			pushNotificationService.test(sendPushRequestDto, email);
 			return GentiResponse.success(true);
 		}
 	}

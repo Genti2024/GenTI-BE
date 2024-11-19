5bdc976cae51d338e69368d7fd62f096b1c79a6c
LeeJaehyung <122717063+LeeJae-H@users.noreply.github.com>
2024-11-19 17:50:45 +0900

    Fix: #198 /reissue에서 발생하는 에러가 html로 응답되는 문제 - 기존의 @Controller를 @RestController로 수정

--- genti-api/src/main/java/com/gt/genti/auth/controller/AuthController.java
+++ genti-api/src/main/java/com/gt/genti/auth/controller/AuthController.java
@@ -42,7 +42,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth/v1")
 public class AuthController implements AuthApi {

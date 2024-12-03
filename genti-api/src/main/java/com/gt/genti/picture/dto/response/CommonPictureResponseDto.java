65919d9351232d05b9bfefeffe356e49cdcac949
LeeJaehyung <540900@naver.com>
2024-12-03 15:29:12 +0900

    Refactor: #209 genti-domain - picture.post 디렉토리(엔티티) 삭제 + 연관 코드 삭제

--- genti-api/src/main/java/com/gt/genti/picture/dto/response/CommonPictureResponseDto.java
+++ genti-api/src/main/java/com/gt/genti/picture/dto/response/CommonPictureResponseDto.java
@@ -31,7 +31,6 @@ public class CommonPictureResponseDto {
 		"PictureCreatedByCreator",
 		"PicturePose",
 		"PictureUserVerification",
-		"PicturePost",
 		"PictureProfile",
 		"PictureUserFace",
 		"ResponseExample"
